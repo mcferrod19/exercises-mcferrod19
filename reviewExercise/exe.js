b6bcfbec3db678c0b12191c04615117e23bd0d60
Cristina Fernandez <mcferrod@gmail.com>
2017-05-03 09:35:33 +0200

    2nd exercise corrected

--- reviewExercise/exe.js
+++ reviewExercise/exe.js
@@ -5,47 +5,59 @@ La función va a devolver el número de veces que está repetido ese número en 
 Por ejemplo, si le pasamos un 1 y [1,3,4,2,1,3,4,5], la función devolverá un 2,
 porque el número 1 aparece dos veces en el array.*/
 
-var numero = ();
-var arrayDeNumeros =[1,3,4,2,1,3,4,5];
+var number = 1;
+var arrayDeNumbers = [1, 3, 4, 2, 1, 3, 4, 5];
 
-function cuentaNumeros(numero, arrayDeNumeros){
-  var numeroRepetido = 0;
-  for (var i=0; i<arrayDeNumeros.length; i++){
-    if(numero === arrayDeNumeros[i]){
-      numeroRepetido= numeroRepetido + ();
+function cuentaNumeros(numbers, arrayDeNumbers) {
+    var repeatnumber = 0;
+    for (var i = 0; i < arrayDeNumbers.length; i++) {
+        if (number === arrayDeNumbers[i]) {
+            repeatnumber = repeatnumber + number;
+        }
     }
-  }
-  return numeroRepetido;
+    return repeatnumber;
 }
-cuentaNumeros(1,4,5,6);
+cuentaNumeros(number, arrayDeNumbers);
 
 /*EXERCISE 2:
 Vamos a crear una función JavaScript que toma un parámetro que es una cadena (texto).
 Si la cadena es de longitud impar, devuelve el caracter central.
 Si la longitud de la cadena es par, devuelve "Solo funciono con cadenas de longitud impar".*/
 
-var cadenaImpar= 1.5
-function caracterCentral(cadena){
-  var cadenaCentral = cadena.length/cadenaImpar;
-  if (cadena.length/2===0){
-    return"Solo funciono con longitud impar"
-  } else {
-    return cadena[cadenaImpar];
-  }
+
+function caracterCentral(string){
+    var emptytstring = string.length === 0;
+    var esPar = string.length % 2 === 0;
+
+    if (emptytstring){
+        return "la cadena no puede estar vacia";
+    }else if (esPar) {
+        return "Solo funciono con longitud impar";
+    } else {
+      var index = (string.length -1)/2;
+      // var middle = Math.floor(string.length / 2);
+      return string[index];
+    }
 }
-caracterCentral("soyUnaCadenaImpar")
+
+var string = "abCde";
+var string1 = "abcd";
+
+console.log("El caracer central de " + string + "es " + caracterCentral(string));
+console.log("El caracer central de " + string1 + "es " + caracterCentral(string1));
+
 
 /*EXERCISE 3:
 Vamos a crear una función que toma un parámetro que es un array de números.
 La función va a devolver un array que contiene sólo los números pares del array original.*/
 
-encuentraPares([1,3,5,6,7,9]);
+encuentraPares([1, 3, 5, 6, 7, 9]);
 
-function encuentraPares(numeros){
-  var par =[];
-  for (i=0; i<numeros.length; i++){
-    if (numeros[i]/2===0) {
-      par[par.length]=numeros[i]
+function encuentraPares(numeros) {
+    var par = [];
+    for (i = 0; i < numeros.length; i++) {
+        if (numeros[i] / 2 === 0) {
+            par[par.length] = numeros[i]
+        }
     }
-  }
-  return par
+    return par
